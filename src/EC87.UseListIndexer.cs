41c5052cb7f70a9182376a7a5b85e825df97de35
Vianney de Bellabre <vdebellabre@c2s.fr>
2024-05-31 08:36:57 +0200

    Fix

--- src/EC87.UseListIndexer.cs
+++ src/EC87.UseListIndexer.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace EcoCode.LiveWarnings;
+﻿namespace EcoCode.LiveWarnings;
 
 internal static class UseListIndexer
 {
--- src/EC91.WithLINQuseWherebeforeOrderby..cs
+++ src/EC91.WithLINQuseWherebeforeOrderby..cs
@@ -1,43 +1,39 @@
-﻿using System.Linq;
-using System.Collections.Generic;
-using static EcoCode.LiveWarnings.MakeTypeSealed.SealableClassesWithInterface;
+﻿namespace EcoCode.LiveWarnings;
 
-namespace EcoCode.LiveWarnings;
-
-[SuppressMessage("Reliability", "EC91:With LINQ use Where before Order by.", Justification = "When you place the 'orderby' before the 'where', LINQ sorts the entire dataset before filtering it. It is more efficient to filter first and then sort by placing the 'where' before the 'orderby'")]
-internal static class WithLINQuseWherebeforeOrderby
+internal static class WithLinqUseWhereBeforeOrderby
 {
-        public static void Test1()
-        {
-            List<int> items = new List<int>();
+    public static void Test1()
+    {
+        var items = new List<int>();
+        var query = items
+            .OrderBy(x => x)
+            .Where(x => x > 10) // EC91
+            .Select(x => x);
+    }
+    public static void Test2()
+    {
+        var items = new List<int>();
+        var query = items
+            .OrderByDescending(x => x)
+            .Where(x => x > 10) // EC91
+            .Select(x => x);
+    }
 
-            var query = items.OrderBy(x => x)
-                    .Where(x => x > 10)
-                    .Select(x => x);
-        }
-        public static void Test2()
-        {
-                List<int> items = new List<int>();
-                
-                var query = items
-                .OrderByDescending(x => x)
-                .Where(x => x > 10)
-                .Select(x => x);
-        }
-        public static void Test3()
-        {
-            List<int> items = new List<int>();
-            var query = from item in items
-                            orderby item
-                            where item > 10
-                            select item;
-        }
-        public static void Test4()
-        {
-            List<int> items = new List<int>();
-            var query = from item in items
-                            orderby item descending
-                            where item > 10
-                            select item;
-        }
+    public static void Test3()
+    {
+        var items = new List<int>();
+        var query = from item in items
+                    orderby item // EC91
+                    where item > 10
+                    select item;
+    }
+
+    public static void Test4()
+    {
+        var items = new List<int>();
+        var query = from item in items
+                    orderby item descending // EC91
+                    where item > 10
+                    select item;
+    }
 }
