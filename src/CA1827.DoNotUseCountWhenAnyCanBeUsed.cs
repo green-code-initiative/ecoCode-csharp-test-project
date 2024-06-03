dac3df712df2639c951d74709ee2f22e092cf502
Florent-LAVAUD <inverser.cents0f@icloud.com>
2024-06-03 10:49:12 +0000

    fix: DoNotUseCountWhenAnyCanBeUsed.M2 static

--- src/CA1827.DoNotUseCountWhenAnyCanBeUsed.cs
+++ src/CA1827.DoNotUseCountWhenAnyCanBeUsed.cs
@@ -7,6 +7,6 @@ internal static class DoNotUseCountWhenAnyCanBeUsed
     public static async Task<string> M1Async(IQueryable<string> list)
         => await list.CountAsync() != 0 ? "Not empty" : "Empty";
 
-    public async Task<string> M2Async(IQueryable<string> list)
+    public static async Task<string> M2Async(IQueryable<string> list)
         => await list.LongCountAsync() > 0 ? "Not empty" : "Empty";
 }
